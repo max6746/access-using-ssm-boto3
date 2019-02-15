195ebbff52266892160c7710eed66afd8baf782a
Kumar Mayank <mayank@flux7.com>
2019-02-16 01:10:20 +0530

    Updated Output format

--- boto3-ssm.py
+++ boto3-ssm.py
@@ -1,7 +1,6 @@
 import boto3, time, sys
 
-#Recursive function to check the status
-#Returns after Success or Failed
+#Returns Output and Status of the Command executed
 def check_status(commandId, instanceId):
     client = boto3.client('ssm')
     response2 = client.get_command_invocation(
@@ -12,6 +11,8 @@ def check_status(commandId, instanceId):
     output = response2['StandardOutputContent']
     return status, output;
 
+
+#Function to exec command and return the command ID
 def command_func(comm, instanceId):
     client = boto3.client('ssm')
     response = client.send_command(
@@ -29,25 +30,25 @@ def command_func(comm, instanceId):
     )
     status = response['Command']['StatusDetails']
     commandId = response['Command']['CommandId']
-
-    #print(status)
     return commandId
 
-#Call func to check status
+
 def main():
     instanceId = 'i-066328aa95bec1ca8'
     for index in range(1,len(sys.argv)):
+        print("********************")
         print("Command: '{0}'".format(sys.argv[index]))
         commandId = command_func(str(sys.argv[index]), instanceId)
         status, output = check_status(commandId,instanceId)
+        #Loop until the terminating state
         while (
                 status == "Pending" or status == "InProgress" or
                 status == "Delayed"):
-            print(status)
             time.sleep(5)
             status, output = check_status(commandId,instanceId)
         print("Output: {0}".format(output))
-        print("Status: {0}".format(status))
+        print("Status: {0}\n".format(status))
+
 
 if __name__ == '__main__':
     main()
